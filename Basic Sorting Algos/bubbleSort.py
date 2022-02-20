3f3c0d2c2110180132c1919dfc89af5e6bfa91e8
chirag.tyagi90 <chirag.tyagi90@outlook.com>
2022-02-20 22:27:21 +0530

    Bubble Sort Implementation without using a temp variable.

--- Basic Sorting Algos/bubbleSort.py
+++ Basic Sorting Algos/bubbleSort.py
@@ -8,5 +8,15 @@ def bubbleSort(list):
                 list[j+1]=temp
     return list
 
-unsortedList=[5, 3, 8, 6, 7, 2]
-print(bubbleSort(unsortedList))
+unsortedList=[9,5, 3, 8, 6, 7, 2]
+
+#Bubble sort implementation Without using a temp variable
+
+def bubbleSortWOtemp(list):
+    for i in range(0,len(list)-1):
+        for j in range(len(list)-1):
+            if(list[j]>list[j+1]):
+                list[j],list[j+1]=list[j+1],list[j]
+    return list
+
+print(bubbleSortWOtemp(unsortedList))
