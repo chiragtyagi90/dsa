c7704ecf479c41854e079f43e88ce80c4f2aa3f8
chirag.tyagi90 <chirag.tyagi90@outlook.com>
2022-02-21 17:23:31 +0530

    Updated the directory structure for the rest of the dsa topics

--- Basic Sorting Algos/scripts/bubbleSort.py
+++ Basic Sorting Algos/scripts/bubbleSort.py
@@ -8,15 +8,22 @@ def bubbleSort(list):
                 list[j+1]=temp
     return list
 
-unsortedList=[9,5, 3, 8, 6, 7, 2]
+unsortedList=[3, 8, 16, 17, 22]
 
 #Bubble sort implementation Without using a temp variable
 
 def bubbleSortWOtemp(list):
+    #count=0
     for i in range(0,len(list)-1):
         for j in range(len(list)-1):
             if(list[j]>list[j+1]):
                 list[j],list[j+1]=list[j+1],list[j]
+     #           count=count+1
+    #print(count)
     return list
 
 print(bubbleSortWOtemp(unsortedList))
+#TODO: Optimization of Python Code Implementation(https://www.javatpoint.com/bubble-sort-in-python)
+
+
+#TODO
---
+++ Basic Sorting Algos/scripts/insertionSort.py
@@ -0,0 +1,7 @@
+def insertionSort(list):
+    list.sort()
+    return list
+
+
+unsortedList=[23,12,3,987,45,-1]
+print(insertionSort(unsortedList))
