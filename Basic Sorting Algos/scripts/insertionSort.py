95072e76e7e2bb67eddd37cd1725d5facbca2172
chirag.tyagi90 <chirag.tyagi90@outlook.com>
2022-02-21 18:35:45 +0530

    Implemented insertionSort

--- Basic Sorting Algos/scripts/insertionSort.py
+++ Basic Sorting Algos/scripts/insertionSort.py
@@ -1,7 +1,12 @@
 def insertionSort(list):
-    list.sort()
+    for i in range(1 ,len(list)):
+        temp=list[i]
+        j=i-1
+        while temp<list[j] and j>-1:
+            list[j+1]=list[j]
+            list[j]=temp
+            j=j-1
     return list
 
-
-unsortedList=[23,12,3,987,45,-1]
+unsortedList=[23,12,3,987,-8,45]
 print(insertionSort(unsortedList))
