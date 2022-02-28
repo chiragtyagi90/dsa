e417017e9a60e56d69db79c05bfd5357215d0f34
chirag.tyagi90 <chirag.tyagi90@outlook.com>
2022-03-01 00:15:06 +0530

    Queue Implementation using Array(ListInPython)

--- Queues/scripts/queuesArray.py
+++ Queues/scripts/queuesArray.py
@@ -2,29 +2,49 @@ class Queues:
     def __init__(self):
         print("Implementing Queues using Python Built In List Class...")
         self.data=[]
-    def enque(self,element):
+    def enque(self):
+        element=input("Enter the element to add to the queue!!!")
         print(f"""Inserting element {element} into the queue""")
         self.data.append(element)
-        print(self.data)
 
     def isNotEmpty(self):
         print("Inside isNotEmpty...")
         return len(self.data)>0
-
     def deque(self):
         print("Inside deque...")
         if(self.isNotEmpty()):
             print(f"""Deleting element {self.data.pop(0)} from the queue""")
         else:
             print("Queue is empty")
-
+    def getQueueElements(self):
+        print(self.data)
+    def getQueueLength(self):
+        print(len(self.data))
+    def top(self):
+        print(self.data[0])
 
 
 q=Queues()
-q.enque(10)
-q.enque(20)
-q.deque()
-q.enque(99)
-q.deque()
-q.deque()
-q.deque()
+mydict = {"1":q.enque,"2":q.getQueueElements,"3":q.deque,"4":q.getQueueLength,"5":q.top}
+while True:
+    print(f"""
+1.Enter 1 to Add elements into the queue!
+2.Enter 2 to view the elements of the queue!
+3.Enter 3 to delete an element from the queue!
+4.Enter 4 to get the length of the queue!
+5.Enter 5 to see the top element of the queue!
+6.Enter 6 to exit the program!
+""")
+    operation=int(input("Enter your choice:"))
+    print(f"""Operation Selected is {operation}""")
+    if(operation==6):
+        break
+    else:
+        mydict[str(operation)]()
+
+print("Queue Demo Finished")
+
+
+
+
+
