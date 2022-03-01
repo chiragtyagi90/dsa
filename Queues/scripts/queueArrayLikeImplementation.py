0c4ef5ddde66aa999f90842242b974594a9b7878
chirag.tyagi90 <chirag.tyagi90@outlook.com>
2022-03-01 11:22:00 +0530

    Menu driven Queue Implementation using Python's built in list

--- Queues/scripts/queueArrayLikeImplementation.py
+++ Queues/scripts/queueArrayLikeImplementation.py
@@ -1,30 +1,40 @@
 class Queue:
     def __init__(self):
         print("Implementing Queues using Python Built In List Class...")
-        print(f"""Here we are tring to mimic the array based implementation.
-So for this example we will restrict of queue to the maximum size which the user selects:
-""")
+        print(f"""Menu Driven Queue implementation""")
         self.data=[]
         self.MaxQueueLength = 0
+        self.firstRun=True
 
     def enque(self):
+
+        #print(f""" self.MaxQueueLength = {self.MaxQueueLength} """)
+        #print(f""" len(data) = {len(self.data)} """)
         if(self.MaxQueueLength==0 or len(self.data)<self.MaxQueueLength):
-            self.MaxQueueLength = int(input("Enter the maximum length of the array!"))
-            for i in range(0,self.MaxQueueLength):
+            if(self.firstRun):
+                self.MaxQueueLength = int(input("Enter the maximum length of the array!"))
+                length=self.MaxQueueLength
+            else:
+                newLength=self.MaxQueueLength-len(self.data)
+                print(f"""Only {newLength} can be added to the queue""")
+                length=newLength
+            for i in range(0,length):
                 self.data.append(input(f"""Enter {i+1} element of the queue!"""))
+                self.firstRun=False
             print(f"""{self.MaxQueueLength} Elements inserted into the Queue!""")
         elif(self.MaxQueueLength>=0):
                 print("Can not insert more Elements as Queue is Full! Delete some elements!!!")
 
     def deque(self):
         self.data.pop(0)
-        self.MaxQueueLength=self.MaxQueueLength-1
+        self.getQueueLength()
     def getQueueElements(self):
         print(self.data)
     def getQueueLength(self):
-        pass
+        print(f"""Length of queue is {len(self.data)}""")
+        print(f"""Empty space remaining in Queue is {self.MaxQueueLength-len(self.data)}""")
     def top(self):
-        pass
+        print(self.data[0])
 
 
 q=Queue()
@@ -37,13 +47,13 @@ while True:
 3.Enter 3 to delete an element from the queue!
 4.Enter 4 to get the length of the queue!
 5.Enter 5 to see the top element of the queue!
-6.Enter 6 to exit the program!
+6.Enter X to exit the program!
 """)
     operation=input("Enter your choice:")
     print(f"""Operation Selected is {operation}""")
-    if(operation=="6"):
+    if(operation.lower()=="x"):
         break
     else:
         mydict[operation]()
 
-print("Queue Demo Finished")
+print("***********************Queue Demo Finished***********************")
